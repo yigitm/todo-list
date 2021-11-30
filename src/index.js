7ecded20befd71459849b812a44a94c278227ee5
Yigit Mersin <yigitmersin@gmail.com>
2021-11-30 23:49:14 +0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-unused-vars */
 import _ from 'lodash';
 import './style.css';
 
 const ulElement = document.getElementsByTagName('ul')[0];
-let tasks = [];
+const tasks = [];
 
 const createTask = () => {
-  for (let index = 0; index < 5; index++) {
+  for (let index = 0; index < 5; index += 1) {
     const liElement = document.createElement('li');
-    let task = {
-      description: 'Task ' + index,
+    const task = {
+      description: `Task ${index}`,
       completed: false,
       id: tasks.length - 1,
     };
 
-    task.id++;
+    task.id += 1;
     tasks.push(task);
     liElement.innerHTML = `<input class="check-box" type="checkbox" name="" id="" /><li>${task.description}</li><i class="fas fa-ellipsis-v fa-1x"></i>`;
     ulElement.appendChild(liElement);
