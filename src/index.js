b57d2841b633e5391e9948fea1b7a311725af9b4
Yigit Mersin <yigitmersin@gmail.com>
2021-11-30 23:23:41 +0300

    Add createTask function & add style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import _ from 'lodash';
 import './style.css';
+
+const ulElement = document.getElementsByTagName('ul')[0];
+let tasks = [];
+
+const createTask = () => {
+  for (let index = 0; index < 5; index++) {
+    const liElement = document.createElement('li');
+    let task = {
+      description: 'Task ' + index,
+      completed: false,
+      id: tasks.length - 1,
+    };
+
+    task.id++;
+    tasks.push(task);
+    liElement.innerHTML = `<input class="check-box" type="checkbox" name="" id="" /><li>${task.description}</li><i class="fas fa-ellipsis-v fa-1x"></i>`;
+    ulElement.appendChild(liElement);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => createTask());
