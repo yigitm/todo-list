ce17514219617252f68fca04a3c977ecf4b2e67c
Yigit Mersin <yigitmersin@gmail.com>
2021-12-03 18:19:01 +0300

    Merge seperaet modules & chang createTask function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './style.css';
 import UI from './modules/ui';
-import Backend from './modules/backend';
 
 document.addEventListener('DOMContentLoaded', () => {
   UI.showTasks();
   const checkbox = document.querySelectorAll('.check-box');
   checkbox.forEach((element) => {
     element.addEventListener('change', (e) => {
-      Backend.updateStatus(e.target);
+      UI.updateStatus(e.target);
       UI.updateCrossLine(e.target);
+      UI.createTaskUI();
     });
   });
-  UI.checkCrossline(Backend.tasks, checkbox);
+  UI.checkCrossline(UI.tasks, checkbox);
   UI.changeDescription();
   UI.filterCompleted(checkbox);
 });
-Backend.enterEvent();
-Backend.arrowEvent();
+UI.enterEvent();
+UI.arrowEvent();
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,7 +1,73 @@
-/* eslint-disable import/no-cycle */
-import Backend from './backend';
+import UI from './ui';
 
-const UI = (() => {
+const Backend = (() => {
+  const setLocal = (tasks) => {
+    localStorage.setItem('Tasks', JSON.stringify(tasks));
+  };
+
+  const getLocal = () => {
+    const localData = JSON.parse(localStorage.getItem('Tasks'));
+    return localData;
+  };
+  // Create task
+  const inputElement = document.getElementById('task-input');
+  let tasks = localStorage.length > 0 ? getLocal() : [];
+  const createTask = () => {
+    const task = {
+      description: inputElement.value,
+      completed: false,
+      index: tasks.length,
+    };
+
+    tasks.push(task);
+    setLocal(tasks);
+    createTaskUI(task);
+  };
+  // Add task when press enter
+  const enterEvent = () => {
+    inputElement.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        createTask();
+        //location.reload();
+      }
+    });
+  };
+  // Add task when click
+  const arrowEvent = () => {
+    const iconElement = document.getElementById('create-task');
+    iconElement.addEventListener('click', () => {
+      createTask();
+    });
+  };
+  // Update value of completed
+  const updateStatus = (checkbox) => {
+    const taskIndex = tasks.findIndex(
+      (task) => task.index === parseInt(checkbox.id, 10),
+    );
+
+    if (tasks[taskIndex].completed) {
+      tasks[taskIndex].completed = false;
+      setLocal(tasks);
+    } else {
+      tasks[taskIndex].completed = true;
+      setLocal(tasks);
+    }
+  };
+  // Update description value
+  const descriptionValue = (span) => {
+    tasks[span.previousSibling.id].description = span.innerText;
+    setLocal(tasks);
+  };
+  // Remove task from local storage & temporary array tasks
+  const taskValue = (span) => {
+    const itemIndex = tasks.findIndex(
+      (task) => task.index === parseInt(span.previousSibling.id, 10),
+    );
+    tasks.splice(itemIndex, 1);
+    setLocal(tasks);
+  };
+  //UI related functions starts from here
   const ulElement = document.getElementsByTagName('ul')[0];
 
   const updateCrossLine = (eTarget) => {
@@ -18,27 +84,33 @@ const UI = (() => {
     if (tasks != null) {
       tasks.forEach((element) => {
         if (element.completed === true) {
-          checkbox[element.index].nextElementSibling.style.textDecoration = 'line-through';
+          checkbox[element.index].nextElementSibling.style.textDecoration =
+            'line-through';
         }
       });
     }
   };
 
   const createTaskUI = (task) => {
-    const liElement = document.createElement('li');
-    liElement.innerHTML = `<input class="check-box" type="checkbox" name="checkbox" id="${
-      task.index
-    }" ${
-      task.completed ? 'checked' : ''
-    }/><span contentEditable="false" id="edit">${task.description}
-    </span><i class="fas fa-ellipsis-v fa-1x"></i>`;
-    ulElement.appendChild(liElement);
+    const taskContainer = document.getElementById('task-container');
+    let allTasks = JSON.parse(localStorage.getItem('Tasks'));
+    let taskLi = '<ul>';
+    allTasks.forEach((task) => {
+      taskLi += `<li><input class="check-box" type="checkbox" name="checkbox" id="${
+        task.index
+      }" ${
+        task.completed ? 'checked' : ''
+      }/><textarea contentEditable="false" id="edit">${task.description}
+    </textarea><i class="fas fa-ellipsis-v fa-1x"></i></li>`;
+    });
+    taskLi += '</ul>';
+    taskContainer.innerHTML = taskLi;
   };
 
   const showTasks = () => {
     if (localStorage.length > 0) {
-      Backend.tasks = Backend.getLocal();
-      Backend.tasks.forEach((task) => {
+      tasks = getLocal();
+      tasks.forEach((task) => {
         createTaskUI(task);
       });
     }
@@ -50,7 +122,7 @@ const UI = (() => {
         e.preventDefault();
         span.nextElementSibling.classList = 'fas fa-ellipsis-v fa-1x';
         span.contentEditable = false;
-        Backend.descriptionValue(span);
+        descriptionValue(span);
         span.style.backgroundColor = null;
       }
     });
@@ -60,7 +132,7 @@ const UI = (() => {
     const deleteIcon = span.nextElementSibling;
     deleteIcon.addEventListener('click', () => {
       span.parentNode.remove();
-      Backend.taskValue(span);
+      taskValue(span);
     });
   };
 
@@ -86,7 +158,7 @@ const UI = (() => {
     const clear = document.querySelector('.clear');
     const filters = [];
     clear.addEventListener('click', () => {
-      Backend.tasks.filter((item) => {
+      tasks.filter((item) => {
         if (item.completed) {
           filters.push(item.index);
         }
@@ -99,6 +171,15 @@ const UI = (() => {
   };
 
   return {
+    tasks,
+    setLocal,
+    getLocal,
+    createTask,
+    enterEvent,
+    arrowEvent,
+    updateStatus,
+    descriptionValue,
+    taskValue,
     updateCrossLine,
     checkCrossline,
     createTaskUI,
@@ -108,4 +189,4 @@ const UI = (() => {
     filterCompleted,
   };
 })();
-export default UI;
+export default Backend;
