79bfd2f0dbd20d709eb192a2b4c3f56bb655837a
Yigit Mersin <yigitmersin@gmail.com>
2021-12-07 01:12:30 +0300

    Add localstorage & creat task & show task & check task features

--- src/index.js
+++ src/index.js
@@ -2,37 +2,18 @@ import './style.css';
 import UI from './modules/ui';
 
 const addIcon = document.getElementById('create-task');
-const taskContainer = document.getElementById('task-container');
-const inputField = document.getElementById('task-input');
 const clear = document.getElementById('clear');
 
+//show local stroge items
+document.addEventListener('DOMContentLoaded', () => {
+  UI.showTasks();
+});
 //event for single task creation
 addIcon.addEventListener('click', () => {
-  // Single task parent
-  const taskWrapper = document.createElement('div');
-  taskWrapper.setAttribute('id', `${UI.tasks.length}`);
-  // create checkbox & its event
-  const checkbox = document.createElement('input');
-  checkbox.classList.add('checked');
-  checkbox.setAttribute('type', 'checkbox');
-  checkbox.setAttribute('id', 'test');
-  checkbox.addEventListener('click', (e) => UI.markTask(e.target));
-  // create textArea for task
-  const textArea = document.createElement('textarea');
-  textArea.setAttribute('id', 'edit');
-  textArea.innerText = inputField.value;
-  // create remove button & its event
-  const iconClass = ['far', 'fa-trash-alt', 'fa-1x'];
-  const delIcon = document.createElement('i');
-  delIcon.classList.add(...iconClass);
-  delIcon.addEventListener('click', (e) => UI.removeTask(e.target));
-  //Add items to task Container
-  taskWrapper.append(checkbox);
-  taskWrapper.append(textArea);
-  taskWrapper.append(delIcon);
-  taskContainer.append(taskWrapper);
+  //Create single task element
+  UI.taskElement(UI.createTask());
 });
-
+//event to filter completed tasks
 clear.addEventListener('click', () => {
   UI.clearTask();
 });
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,11 +1,60 @@
 const UI = (() => {
-  const task = {
-    description: '',
-    completed: '',
-    index: '',
+  let tasks = [];
+  const taskContainer = document.getElementById('task-container');
+  const inputField = document.getElementById('task-input');
+
+  const getLocal = () => {
+    const localData = JSON.parse(localStorage.getItem('Tasks'));
+    return localData;
   };
 
-  const tasks = [];
+  const setLocal = (tasks) => {
+    localStorage.setItem('Tasks', JSON.stringify(tasks));
+  };
+
+  const createTask = () => {
+    const task = {
+      description: inputField.value,
+      completed: false,
+      index: tasks.length,
+    };
+    tasks.push(task);
+    setLocal(tasks);
+    return task;
+    //console.log(tasks);
+  };
+
+  const taskElement = (task) => {
+    // Single task parent
+    const taskWrapper = document.createElement('div');
+    taskWrapper.setAttribute('id', `${task.index}`);
+    // create checkbox & its event
+    const checkbox = document.createElement('input');
+    checkbox.classList.add('checked');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.addEventListener('click', (e) => markTask(e.target));
+    // create textArea for task
+    const textArea = document.createElement('textarea');
+    textArea.setAttribute('id', 'edit');
+    textArea.innerText = task.description;
+    // create remove button & its event
+    const iconClass = ['far', 'fa-trash-alt', 'fa-1x'];
+    const delIcon = document.createElement('i');
+    delIcon.classList.add(...iconClass);
+    delIcon.addEventListener('click', (e) => UI.removeTask(e.target));
+    //Add items to task Container
+    taskWrapper.append(checkbox);
+    taskWrapper.append(textArea);
+    taskWrapper.append(delIcon);
+    taskContainer.append(taskWrapper);
+  };
+
+  const showTasks = () => {
+    localStorage.length !== 0 ? (tasks = getLocal()) : false;
+    tasks.forEach((task) => {
+      taskElement(task);
+    });
+  };
 
   const markTask = (eTarget) => {
     eTarget.checked
@@ -18,11 +67,22 @@ const UI = (() => {
   };
 
   const clearTask = () => {
-    const checkedItems = document.querySelectorAll('.checked');
-    checkedItems.forEach((item) => {
-      item.checked ? (item.parentNode.style.display = 'none') : false;
-    });
+    let checkedItems = document.querySelectorAll('.checked');
+    let [...allChecks] = checkedItems;
+    allChecks
+      .filter((item) => item.checked === true)
+      .forEach((item) => {
+        item.parentNode.style.display = 'none';
+      });
   };
-  return { task, tasks, markTask, removeTask, clearTask };
+  return {
+    tasks,
+    taskElement,
+    createTask,
+    showTasks,
+    markTask,
+    removeTask,
+    clearTask,
+  };
 })();
 export default UI;
